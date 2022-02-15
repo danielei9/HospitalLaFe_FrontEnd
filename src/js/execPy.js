9fef537a05a82711a44d6a6930b0c76374b35408
danielei9 <dbs99@hotmail.com>
2022-02-16 00:48:53 +0100

    Update execPy.js

--- src/js/execPy.js
+++ src/js/execPy.js
@@ -1,5 +1,5 @@
 import  exec from 'child_process'
-export function execPython() {
+module.exports =  function execPython() {
 //const { exec } = require('child_process');
 
 exec('python C:/Users/dbs99/Desktop/Proyectos Buchu/HospitalLaFe_FrontEnd/Python/hospiPython.py', (error, stdout, stderr) => {
