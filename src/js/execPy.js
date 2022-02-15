285ac3b541738c9e0f0dc2b6495b4b5ea247bad6
danielei9 <dbs99@hotmail.com>
2022-02-16 00:34:39 +0100

    upd

---
+++ src/js/execPy.js
@@ -0,0 +1,16 @@
+const { exec } = require('child_process');
+function execPython() {
+exec('python C:/Users/dbs99/Desktop/Proyectos Buchu/HospitalLaFe_FrontEnd/Python/hospiPython.py', (error, stdout, stderr) => {
+    if (error) {
+      console.error(`error: ${error.message}`);
+      return;
+    }
+  
+    if (stderr) {
+      console.error(`stderr: ${stderr}`);
+      return;
+    }
+  
+    console.log(`stdout:\n${stdout}`);
+  });
+}
