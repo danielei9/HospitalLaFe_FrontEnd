9e76bd8c953c8e08240335f0b60469cb09b92ece
danielei9 <dbs99@hotmail.com>
2022-02-16 19:04:52 +0100

    update working webservice 7878

--- src/js/execPy.js
+++ src/js/execPy.js
@@ -1,7 +1,6 @@
-import  exec from 'child_process'
-module.exports =  function execPython() {
 //const { exec } = require('child_process');
-
+import * as exec from 'child_process'
+export function execPython() {
 exec('python C:/Users/dbs99/Desktop/Proyectos Buchu/HospitalLaFe_FrontEnd/Python/hospiPython.py', (error, stdout, stderr) => {
     if (error) {
       console.error(`error: ${error.message}`);
@@ -16,3 +15,4 @@ exec('python C:/Users/dbs99/Desktop/Proyectos Buchu/HospitalLaFe_FrontEnd/Python
     console.log(`stdout:\n${stdout}`);
   });
 }
+execPython()
